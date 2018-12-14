3ac75416a5340fbb628652cc10e6486ccf8b4742
James <conjam@umich.edu>
2018-12-14 16:51:44 -0500

    matmult updated

--- mat_mult.c
+++ mat_mult.c
@@ -2,6 +2,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int n = 64;
+
+
+
+
+
+void kernel(int  mat_a[n][n],int  mat_b[n][n], int partial[n][n][n], int result[n][n]) { 
+    int i = 0, j = 0, k = 0;  
+    //multiply
+    for (i = 0; i < n; ++i) {
+    	for (j = 0; j < n; ++j) {
+    		for (k = 0; k < n; ++k) {
+    			partial[i][j][k] = mat_a[j][i] * mat_b[i][k];
+    	//		printf("partial is %i, i is %i, j is %i, k is %i\n", partial[i][j][k], i, j, k);
+    		}
+    	}
+    }
+    //acc
+    for (i = 0; i < n; ++i) {
+    	for (j = 0; j < n; ++j) {
+    		for (k = 0; k < n; ++k) {
+    			result[i][j] += partial[k][i][j];
+    		}
+    	}
+    }
+}
+
+
+
 int main() {
 	int n = 64;
 	int i = 0, j = 0, k = 0;
@@ -14,26 +43,6 @@ int main() {
 		}
 	}
 
-begin:
-	//multiply
-	for (i = 0; i < n; ++i) {
-		for (j = 0; j < n; ++j) {
-			for (k = 0; k < n; ++k) {
-				partial[i][j][k] = mat_a[j][i] * mat_b[i][k];
-		//		printf("partial is %i, i is %i, j is %i, k is %i\n", partial[i][j][k], i, j, k);
-			}
-		}
-	}
-	//acc
-	for (i = 0; i < n; ++i) {
-		for (j = 0; j < n; ++j) {
-			for (k = 0; k < n; ++k) {
-				result[i][j] += partial[k][i][j];
-			}
-		}
-	}
+    kernel(mat_a,mat_b,partial,result);
 
-end:
-	return 0;
 }
-
