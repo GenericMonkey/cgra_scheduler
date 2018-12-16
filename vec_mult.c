e156bc5db37aad98986383c919413454ff7b30d2
James <conjam@umich.edu>
2018-12-16 18:42:46 -0500

    updated vec_mult

--- vec_mult.c
+++ vec_mult.c
@@ -4,14 +4,16 @@
 
 int n = 64;
 
-void kernel(int  vec_a[n],int  vec_b[n], int result[n]) { 
+int kernel(int  vec_a[n],int  vec_b[n], int result[n]) { 
     int i = 0, j = 0, k = 0;  
 	int dot = 0;
+    int numiter = n;
     //multiply
-    for (i = 0; i < n; ++i) {
+    for (i = 0; i < numiter; ++i) {
     	result[i] = vec_a[i] * vec_b[i];
 		dot += result[i];
     }
+    return dot;
 }
 
 
